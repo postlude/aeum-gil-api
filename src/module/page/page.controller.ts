62acff6afa2a1389c6803c395e5a1ae6215b52fd
Asher Han <sdhan@zigbang.com>
2024-10-30 23:55:22 +0900

    feat: add get page api

--- src/module/page/page.controller.ts
+++ src/module/page/page.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { PageService } from './page.service';
 
 @Controller('/pages')
@@ -6,4 +6,11 @@ export class PageController {
 	constructor(
 		private readonly pageService: PageService
 	) {}
+
+	@Get('/:pageId')
+	public async getPage(
+		@Param('pageId', ParseIntPipe) pageId: number
+	) {
+		return await this.pageService.getPage(pageId);
+	}
 }
--- src/module/page/page.service.ts
+++ src/module/page/page.service.ts
@@ -1,5 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PageRepository } from 'src/database/repository/page.repository';
 
 @Injectable()
 export class PageService {
+	constructor(
+		private readonly pageRepository: PageRepository
+	) {}
+
+	public async getPage(pageId: number) {
+		const page = await this.pageRepository.findOneBy({ id: pageId });
+		if (!page) {
+			throw new NotFoundException();
+		}
+		return page;
+	}
 }
