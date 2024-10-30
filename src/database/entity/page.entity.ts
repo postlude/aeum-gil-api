20067699bac606885d5c1c33cb9c989340172a73
Asher Han <sdhan@zigbang.com>
2024-10-30 23:55:10 +0900

    fix: add column name

--- src/database/entity/page.entity.ts
+++ src/database/entity/page.entity.ts
@@ -9,7 +9,7 @@ export class Page {
 	@PrimaryGeneratedColumn({ type: 'int', unsigned: true })
 	public id: number;
 
-	@Column({ type: 'int', unsigned: true, nullable: true, comment: '현재 페이지와 연결된 이전 페이지 id' })
+	@Column({ type: 'int', name: 'prev_page_id', unsigned: true, nullable: true, comment: '현재 페이지와 연결된 이전 페이지 id' })
 	public prevPageId: number | null;
 
 	@Column({ type: 'varchar', length: 300, nullable: true, comment: '페이지 설명' })
