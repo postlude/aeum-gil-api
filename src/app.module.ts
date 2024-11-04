056619ae3a66a2a56b2e5244ff335178ab21d8f5
postlude <seungdeokhan@naver.com>
2024-11-05 00:10:09 +0900

    feat: apply config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { DatabaseModule } from './database/database.module';
 import { PageModule } from './module/page/page.module';
 import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
 	imports: [
+		// When a key exists both in the runtime environment as an environment variable and in a .env file, the runtime environment variable takes precedence.
+		ConfigModule.forRoot({
+			envFilePath: [ 'src/config/local.env' ],
+			isGlobal: true
+		}),
 		DatabaseModule,
 		PageModule
 	],
