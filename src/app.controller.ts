530f46ad3348673af3ba71249aa349d7283adf5e
postlude <seungdeokhan@naver.com>
2024-11-19 14:40:44 +0900

    feat: add health check api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-	@Get()
-	public getHello(): string {
-		return 'hello world';
+	@Get('/health')
+	public checkHealth() {
+		return 'ok';
 	}
 }
