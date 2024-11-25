902c315e232b3d5ba341aa5db09e990fd101bd71
postlude <seungdeokhan@naver.com>
2024-11-25 17:43:29 +0900

    chore: add log

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initializeTransactionalContext } from 'typeorm-transactional';
+import { ConfigService } from '@nestjs/config';
+import { MySqlConfig } from './config/config.model';
 
 async function bootstrap() {
 	initializeTransactionalContext();
@@ -14,7 +16,11 @@ async function bootstrap() {
 
 	await app.listen(port);
 
+	const mysqlHost = app.get(ConfigService<MySqlConfig>)
+		.get('MYSQL_HOST', { infer: true });
+
 	console.log('[AEUM-GIL API]');
 	console.log(`PORT : ${port}`);
+	console.log(`MySQL Host : ${mysqlHost}`);
 }
 bootstrap();
