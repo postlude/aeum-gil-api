1c9a75c6c5fbb861736141acc76c79e0783b1109
postlude <seungdeokhan@naver.com>
2024-11-30 21:14:15 +0900

    feat: update entity

--- src/database/entity/page.entity.ts
+++ src/database/entity/page.entity.ts
@@ -4,24 +4,30 @@ import {
 	PrimaryGeneratedColumn
 } from 'typeorm';
 
-@Entity({ database: 'aeum_gil', name: 'page' })
+@Entity({ database: 'aeum_gil', name: 'page', comment: '페이지' })
 export class Page {
 	@PrimaryGeneratedColumn({ type: 'int', unsigned: true })
 	public id: number;
 
-	@Column({ type: 'int', name: 'prev_page_id', unsigned: true, nullable: true, comment: '현재 페이지와 연결된 이전 페이지 id' })
-	public prevPageId: number | null;
+	@Column({ name: 'next_page_id', type: 'int', unsigned: true, nullable: true, comment: '선택지가 없는 타입인 경우 다음 페이지 id' })
+	public nextPageId: number | null;
 
 	@Column({ type: 'varchar', length: 300, nullable: true, comment: '페이지 설명' })
 	public description: string | null;
 
+	@Column({ type: 'tinyint', unsigned: true, comment: '1: content only, 2: 선택지 존재' })
+	public type: number;
+
+	@Column({ type: 'varchar', length: 200, comment: '제목' })
+	public title: string;
+
 	@Column({ type: 'text', comment: '본문' })
 	public content: string;
 
-	@Column({ type: 'datetime', name: 'created_at' })
+	@Column({ name: 'created_at', type: 'datetime' })
 	public createdAt: Date;
 
-	@Column({ type: 'datetime', name: 'updated_at' })
+	@Column({ name: 'updated_at', type: 'datetime' })
 	public updatedAt: Date;
 
 	// @ManyToOne(() => Page, (page) => page.id, { nullable: true })
