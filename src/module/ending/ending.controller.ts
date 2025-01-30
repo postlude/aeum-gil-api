a5aa7426df7355aeb78464b14cb7bc1b07b8e15e
postlude <seungdeokhan@naver.com>
2025-01-30 21:36:30 +0900

    feat: add ending update api

--- src/module/ending/ending.controller.ts
+++ src/module/ending/ending.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { EndingService } from './ending.service';
 import { SaveEndingBody } from './ending.dto';
@@ -17,6 +17,17 @@ export class EndingController {
 		@Body() body: SaveEndingBody
 	) {
 		const endingId = await this.endingService.saveEnding(body);
+		return endingId;
+	}
+
+	@Put('/:endingId')
+	@ApiOperation({ summary: '엔딩 수정' })
+	@ApiResponse({ status: HttpStatus.OK, type: Number, description: '수정된 ending.id' })
+	public async setEnding(
+		@Param('endingId', ParseIntPipe) endingId: number,
+		@Body() body: SaveEndingBody
+	) {
+		await this.endingService.saveEnding(body, endingId);
 		return endingId;
 	}
 }
