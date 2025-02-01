31dfffc55b74a766a2a2e0a456c075080101e141
postlude <seungdeokhan@naver.com>
2025-02-01 23:15:31 +0900

    feat: add error interceptor
    - send message to discord when 500 error occured

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { DatabaseModule } from './database/database.module';
 import { PageModule } from './module/page/page.module';
-import { APP_PIPE } from '@nestjs/core';
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { ChoiceOptionModule } from './module/choice-option/choice-option.module';
 import { ItemModule } from './module/item/item.module';
 import { FileModule } from './module/file/file.module';
 import { EndingModule } from './module/ending/ending.module';
+import { ErrorInterceptor } from './core/error.interceptor';
+import { DiscordService } from './util/discord.service';
 
 @Module({
 	imports: [
@@ -28,7 +30,12 @@ import { EndingModule } from './module/ending/ending.module';
 		{
 			provide: APP_PIPE,
 			useValue: new ValidationPipe({ transform: true })
-		}
+		},
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: ErrorInterceptor
+		},
+		DiscordService
 	]
 })
 export class AppModule {}
