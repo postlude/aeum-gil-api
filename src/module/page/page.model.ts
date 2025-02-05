142739e36dcbb3ff7bc70bd489cd339b833be377
postlude <seungdeokhan@naver.com>
2025-02-05 23:27:25 +0900

    feat: add chapterId to post, put api

--- src/module/page/page.model.ts
+++ src/module/page/page.model.ts
@@ -1,8 +1,14 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
-import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+import { IsInt, IsNotEmpty, IsOptional, IsString, MaxLength, Min } from 'class-validator';
 
 export class PageInfo {
+	@ApiPropertyOptional({ type: Number, description: '챕터 id', minimum: 1 })
+	@Expose()
+	@IsInt()
+	@Min(1)
+	public chapterId?: number;
+
 	@ApiPropertyOptional({ type: String, nullable: true, description: '페이지 설명. 유저에게 노출되지 않음' })
 	@Expose()
 	@IsOptional()
