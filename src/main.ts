9b88f140479ecfb611afec6af412d8ba6dc3bcec
postlude <seungdeokhan@naver.com>
2025-02-06 22:54:28 +0900

    chore: version up

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 
 	const swaggerConfig = new DocumentBuilder()
 		.setTitle('에움길 API')
-		.setVersion('0.8.0')
+		.setVersion('0.9.0')
 		.build();
 	const document = SwaggerModule.createDocument(app, swaggerConfig, {
 		operationIdFactory: (controllerKey: string, methodKey: string) => methodKey
