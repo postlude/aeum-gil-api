83cee39552a672868e73aec542dfc54a9490b2c8
postlude <seungdeokhan@naver.com>
2025-03-03 23:22:03 +0900

    feat: add sign-up api

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,9 +1,23 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthDto } from './auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('/auth')
 @ApiTags('Auth')
 export class AuthController {
 	constructor(
+		private readonly authService: AuthService
 	) {}
+
+	@Post('/sign-up')
+	@ApiOperation({ summary: '회원가입' })
+	@ApiResponse({ status: HttpStatus.CREATED, type: Number, description: '생성된 user.id' })
+	@ApiResponse({ status: HttpStatus.CONFLICT, description: '이름 중복' })
+	public async signUp(
+		@Body() { name, password }: AuthDto
+	) {
+		const userId = await this.authService.signUp(name, password);
+		return userId;
+	}
 }
--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -1,7 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { UserRepository } from 'src/database/repository/user.repository';
+import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
 	constructor(
+		private readonly userRepository: UserRepository
 	) {}
+
+	public async signUp(name: string, password: string) {
+		const count = await this.userRepository.countBy({ name });
+		if (count) {
+			throw new ConflictException('이미 존재하는 이름입니다.');
+		}
+
+		const encrypted = await argon2.hash(password);
+
+		const { identifiers } = await this.userRepository.insert({
+			name,
+			password: encrypted
+		});
+
+		return identifiers[0].id as number;
+	}
 }
