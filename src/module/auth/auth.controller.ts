562429bce956597543c236cc38d4f06240d4222b
postlude <seungdeokhan@naver.com>
2025-03-03 23:59:06 +0900

    feat: modify sign-up, sign-in apis
    - jwt token 리턴하도록 변경
    - 토큰 만료는 1일

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -12,23 +12,23 @@ export class AuthController {
 
 	@Post('/sign-up')
 	@ApiOperation({ summary: '회원가입' })
-	@ApiResponse({ status: HttpStatus.CREATED, type: Number, description: '생성된 user.id' })
+	@ApiResponse({ status: HttpStatus.CREATED, type: String, description: 'jwt access token' })
 	@ApiResponse({ status: HttpStatus.CONFLICT, description: '이름 중복' })
 	public async signUp(
 		@Body() { name, password }: AuthDto
 	) {
-		const userId = await this.authService.signUp(name, password);
-		return userId;
+		const accessToken = await this.authService.signUp(name, password);
+		return accessToken;
 	}
 
 	@Post('/sign-in')
 	@ApiOperation({ summary: '로그인' })
-	@ApiResponse({ status: HttpStatus.OK, type: Number, description: '로그인 성공' })
+	@ApiResponse({ status: HttpStatus.OK, type: String, description: 'jwt access token' })
 	@ApiResponse({ status: HttpStatus.UNAUTHORIZED, description: '로그인 실패(이름이 없거나 비번 불일치)' })
 	public async signIn(
 		@Body() { name, password }: AuthDto
 	) {
-		const userId = await this.authService.signIn(name, password);
-		return userId;
+		const accessToken = await this.authService.signIn(name, password);
+		return accessToken;
 	}
 }
--- src/module/auth/auth.module.ts
+++ src/module/auth/auth.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtConfig, JwtExpiration } from 'src/config/config.model';
 
 @Module({
+	imports: [
+		JwtModule.registerAsync({
+			inject: [ ConfigService ],
+			useFactory: (configService: ConfigService<JwtConfig>) => ({
+				secret: configService.get('JWT_SECRET', { infer: true }),
+				signOptions: { expiresIn: JwtExpiration }
+			})
+		})
+	],
 	controllers: [ AuthController ],
 	providers: [ AuthService ]
 })
