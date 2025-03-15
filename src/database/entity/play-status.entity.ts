899837a5272c639421f138d724cfebcffdba521d
postlude <seungdeokhan@naver.com>
2025-03-15 23:06:52 +0900

    feat: update entity

--- src/database/entity/play-status.entity.ts
+++ src/database/entity/play-status.entity.ts
@@ -4,11 +4,32 @@ import {
 	PrimaryColumn
 } from 'typeorm';
 import { MoveTargetType } from './choice-option.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose, Type } from 'class-transformer';
 
-export interface GameStatus {
-	moveTargetType: MoveTargetType;
-	targetId: number;
-	items: { itemId: number; count: number; }[];
+class OwnedItem {
+	@ApiProperty({ type: Number, description: '아이템 id' })
+	@Expose()
+	public itemId: number;
+
+	@ApiProperty({ type: Number, description: '아이템 수량' })
+	@Expose()
+	public count: number;
+}
+
+export class GameStatus {
+	@ApiProperty({ enum: MoveTargetType, enumName: 'MoveTargetType', description: '선택지 선택시 이동할 대상(1: 페이지, 2: 엔딩)' })
+	@Expose()
+	public moveTargetType: MoveTargetType;
+
+	@ApiProperty({ type: Number, description: '다음 페이지 id or 엔딩 id', minimum: 1 })
+	@Expose()
+	public targetId: number;
+
+	@ApiProperty({ type: [ OwnedItem ], description: '소지 아이템' })
+	@Type(() => OwnedItem)
+	@Expose()
+	public ownedItems: OwnedItem[];
 }
 
 @Entity({ database: 'aeum_gil', name: 'play_status', comment: '유저의 현재 플레이 상태' })
@@ -16,8 +37,8 @@ export class PlayStatus {
 	@PrimaryColumn({ name: 'user_id', type: 'int', unsigned: true })
 	public userId: number;
 
-	@Column({ name: 'status', type: 'json', comment: '다음 플레이할 페이지, 현재 소지 아이템 등' })
-	public gameStatus: GameStatus[];
+	@Column({ name: 'game_status', type: 'json', comment: '다음 플레이할 페이지, 현재 소지 아이템 등' })
+	public gameStatus: GameStatus;
 
 	@Column({ name: 'created_at', type: 'datetime' })
 	public createdAt: Date;
