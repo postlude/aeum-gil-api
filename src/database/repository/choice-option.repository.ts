b82b78dfe7877804d0313ecb89dd77a0c1049935
postlude <seungdeokhan@naver.com>
2025-03-20 02:00:31 +0900

    feat: add findOneWithItemMappings

--- src/database/repository/choice-option.repository.ts
+++ src/database/repository/choice-option.repository.ts
@@ -7,4 +7,15 @@ export class ChoiceOptionRepository extends Repository<ChoiceOption> {
 	constructor(private dataSource: DataSource) {
 		super(ChoiceOption, dataSource.createEntityManager());
 	}
+
+	public async findOneWithItemMappings(pageId: number, choiceOptionId: number) {
+		return await this.createQueryBuilder('co')
+			.comment('ChoiceOptionRepository.findOneWithItemMappings')
+			.select([ 'co.moveTargetType', 'co.targetId', 'coim.itemId', 'coim.actionType' ])
+			.innerJoin('co.page', 'p')
+			.leftJoin('co.choiceOptionItemMappings', 'coim')
+			.where('p.id = :pageId', { pageId })
+			.andWhere('co.id = :choiceOptionId', { choiceOptionId })
+			.getOne();
+	}
 }
