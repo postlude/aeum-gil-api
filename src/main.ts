ea748da386df50f81cf150b8422ab2628e48febc
postlude <seungdeokhan@naver.com>
2025-04-20 19:35:25 +0900

    chore: version up

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 
 	const swaggerConfig = new DocumentBuilder()
 		.setTitle('에움길 API')
-		.setVersion('0.20.1')
+		.setVersion('0.21.0')
 		.addBearerAuth()
 		.build();
 	const document = SwaggerModule.createDocument(app, swaggerConfig, {
