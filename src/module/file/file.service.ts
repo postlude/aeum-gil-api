53ef560f222066146684de25fa326d0383789353
postlude <seungdeokhan@naver.com>
2025-06-18 23:02:57 +0900

    refactor: version up aws sdk to v3

--- src/module/file/file.service.ts
+++ src/module/file/file.service.ts
@@ -1,36 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { ImageType } from './file.model';
-import * as AWS from 'aws-sdk';
 import { ConfigService } from '@nestjs/config';
 import dayjs from 'dayjs';
 import { AWSConfig } from 'src/config/config.model';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 @Injectable()
 export class FileService {
 	constructor(
 		private configService: ConfigService<AWSConfig>
 	) {
-		const accessKeyId = this.configService.get('AWS_ACCESS_KEY_ID', { infer: true });
-		const secretAccessKey = this.configService.get('AWS_SECRET_ACCESS_KEY', { infer: true });
+		let accessKeyId = this.configService.get('AWS_ACCESS_KEY_ID', { infer: true });
+		let secretAccessKey = this.configService.get('AWS_SECRET_ACCESS_KEY', { infer: true });
 
-		this.s3 = new AWS.S3({
-			accessKeyId,
-			secretAccessKey,
-			region: 'ap-northeast-2'
+		if (!accessKeyId || !secretAccessKey) {
+			if (process.env.NODE_ENV === 'local') {
+				accessKeyId = '';
+				secretAccessKey = '';
+			} else {
+				throw new Error('AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY is not set');
+			}
+		}
+
+		this.s3Client = new S3Client({
+			region: 'ap-northeast-2',
+			credentials: {
+				accessKeyId,
+				secretAccessKey
+			}
 		});
 	}
 
-	private s3: AWS.S3;
+	private s3Client: S3Client;
 	private readonly PRESIGNED_URL_EXPIRES = 3 * 60; // 3분
 	private readonly S3_BUCKET = 'static.aeum-gil.com';
 
 	public async getPresignedUrl(fileName: string, imageType: ImageType) {
 		const uploadPath = this.getUploadPath(fileName, imageType);
 
-		return await this.s3.getSignedUrlPromise('putObject', {
+		const command = new PutObjectCommand({
 			Bucket: this.S3_BUCKET,
 			Key: uploadPath,
-			Expires: this.PRESIGNED_URL_EXPIRES
+		});
+
+		return await getSignedUrl(this.s3Client, command, {
+			expiresIn: this.PRESIGNED_URL_EXPIRES
 		});
 	}
 
